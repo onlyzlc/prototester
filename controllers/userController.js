71980e208a24a066c82e076bfbb65778aa9a0ebe
onlyzlc <zhoulongchun@live.com>
2020-02-12 23:38:11 +0800

    更新登录和注册接口

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,11 +7,24 @@ exports.register = function(req,res){
         mobile: req.body.mobile
     })
     user.save(function(err,doc){
-        
+        if(err) throw err;
+        res.sendStatus(201);
     })
-    res.send("连接到注册接口")
 }
 
-exports.login = function (req,res) {  
-    res.send("链接到登录接口")
+exports.login = function (req,res) {
+    if(!req.body.hasOwnProperty("email")) return;
+    if(typeof(req.body.email) !== "string") return;
+    User.findOne({"email":req.body.email},function(err,user){
+        if(err) throw err;
+        if(user){
+            if(req.body.password === user.password){
+                res.sendStatus(200);
+            }else{
+                res.send("密码错误");
+            }
+        }else{
+            res.send("用户不存在");
+        }
+    })
 }
