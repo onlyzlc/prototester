297344fea28b8d07fd4420f208f3daf9420db98c
onlyzlc <zhoulongchun@live.com>
2020-03-09 23:10:20 +0800

    修改任务列表

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
     $(".publish").click(function () {
         let taskId = $(this).parent("li").attr("data-taskId");
-        let currentStatus = $(this).parent("li").attr("data-status");
-        let s = (currentStatus==="unpublished")?"published":"unpublished";
+       	let cur_status = $(this).parent("li").attr("data-status");
         $.ajax({
             type: "patch",
             url: "/tasks/"+taskId+"/status",
-            data: {status:s},
-            success: function (response) {
-                $(`li[data-taskId=${taskId}]>.status`).text((response=="unpublished")?("未发布"):("已发布"));
-                $(`li[data-taskId=${taskId}]>.publish`).text((response=="unpublished")?("发布"):("撤下"));
-                $(`li[data-taskId=${taskId}]`).attr("data-status",response);
+            data: {
+            	status: (cur_status === "published")?"unpublished":"published"
+            },
+            secuess: function (response) {
+            	console.info(response);
+            },
+            error: function (response) {
+            	console.error(response);  
             }
         });
     })
+  
 });
