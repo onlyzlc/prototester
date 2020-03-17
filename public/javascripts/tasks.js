8e687856392d3115b2df6b0954cda5106660ba39
zhoulongchun <longchun_zhou@126.com>
2020-03-17 09:04:40 +0800

    修复任务发布功能

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
     $(".publish").click(function () {
-        let taskId = $(this).parent("li").attr("data-taskId");
-       	let cur_status = $(this).parent("li").attr("data-status");
+        let $this = $(this);
+        let $thisItem = $this.parent("li");
+        let taskId = $thisItem.attr("data-taskId");
+       	let cur_status = $thisItem.attr("data-status");
         $.ajax({
             type: "patch",
             url: "/tasks/"+taskId+"/status",
             data: {
             	status: (cur_status === "published")?"unpublished":"published"
             },
-            secuess: function (response) {
-            	console.info(response);
+            success: function (response) {
+                console.info(response);
+                $this.text(response==="published"?"撤下":"发布");
+                $thisItem.attr("data-status",response)
+                    .children(".status").text(response==="published"?"已发布":"已撤下")
+                
             },
             error: function (response) {
             	console.error(response);  
