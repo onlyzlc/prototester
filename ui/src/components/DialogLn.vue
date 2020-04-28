2d9bbc6d4ff440d8eb08841b732b235588d37f9d
onlyzlc <zhoulongchun@live.com>
2020-04-28 23:19:21 +0800

    VUE单文件组件-对话框实现

--- ui/src/components/DialogLn.vue
+++ ui/src/components/DialogLn.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="dialog_container" v-show="visibility">
+    <div class="dialog_container" v-if="vv">
         <div class="dialog_panel">
-            <h3>{{title}}</h3>
+            <div class="dialog_header">
+                <h3>{{title}}</h3>
+                <i class="close" @click="$emit('cancel')"></i>
+            </div>
             <div class="dialog_content">
                 <slot></slot>
             </div>
@@ -11,8 +14,8 @@
                 </div>
                 <div class="right"> 
                     <slot name="foot_right">
-                        <button @click="visibility=false">确定</button>
-                        <button @click="visibility=false">取消</button>
+                        <button @click="$emit('confirm')">确定</button>
+                        <button @click="$emit('cancel')">取消</button>
                     </slot>
                 </div>
             </div>
@@ -22,20 +25,23 @@
 
 <script>
 export default {
+    name: "dialog-ln",
     props:{
         title:{
             type: String,
             default: "对话框"
         },
-        visibility: {
-            type: Boolean,
-            default: true
-        },
+        vv:{
+            type:Boolean,
+            default:true
+        }
     }
+
 }
 </script>
 
 <style scoped>
+    
     .dialog_container{
         position: fixed;
         height: 100%;
@@ -58,12 +64,25 @@ export default {
         display: flex;
         flex-flow: column;
     }
-    .dialog_content {
+    .dialog_header{
+        display: flex;
+        flex-flow: row;
+        align-items: center;
+    }
+    .dialog_header h3{
+        margin: 0;
         flex-grow: 1;
     }
-    .dialog_panel h3{
-        margin: 0;
+    .dialog_header i.close{
+        width: 20px;
+        height: 20px;
+        background-color: bisque;
     }
+    .dialog_content {
+        padding: 1em 0;
+        flex-grow: 1;
+    }
+    
     .dialog_foot{
         display: flex;
         flex-flow: row;
