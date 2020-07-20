81e3e1dd32847de52af5c86d51130db4540c6404
Lonnie <biubiu0_0@163.com>
2020-07-20 19:09:46 +0800

    eslint格式(还有错误)

--- ui/src/views/LoginReg.vue
+++ ui/src/views/LoginReg.vue
@@ -1,26 +1,50 @@
 <template>
   <main>
-        <img src="" alt="">
-        <form action="post">
-            <div>
-                <label for="usersEmail">邮箱</label>
-                <input type="email" autocomplete="username" name="usersEmail" id="usersEmail" required>
-            </div>
-            <div>
-                <label for="userPassword">密码</label>
-                <input type="password" autocomplete="current-password" name="userPassword" id="userPassword" required>
-            </div>
-            <button type="submit">提交</button>
-            <p>已有账号？<a href="/login">去登录</a></p>
-        </form>
-    </main>
+    <img src="" alt="" />
+    <form @submit.prevent="submit">
+      <div>
+        <label for="email">邮箱</label>
+        <input
+          v-model="user.email"
+          type="email"
+          autocomplete="username"
+          name="email"
+          id="email"
+          required
+        />
+      </div>
+      <div>
+        <label for="pw">密码</label>
+        <input
+          v-model="user.pw"
+          type="password"
+          autocomplete="current-password"
+          name="pw"
+          id="pw"
+          required
+        />
+      </div>
+      <button type="submit">提交</button>
+      <p>已有账号？<a href="/login">去登录</a></p>
+    </form>
+  </main>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-}
+  data: {
+    user: {
+      email: "",
+      pw: ""
+    }
+  },
+  methods: {
+    submit: function() {
+      axios.post("/login", this.user).then();
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
