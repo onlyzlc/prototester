2d7fbbb1b008c88fee12b3111f1f513ecab7808d
onlyzlc <zhoulongchun@live.com>
2020-07-26 23:41:54 +0800

    代理和跨域设置

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -3,19 +3,13 @@
 module.exports = {
   outputDir: '../public',
   devServer: {
-    // host: 'localhost',//target host
-    // port: 8080,
+    open: true,
+    port: 8080,
     proxy:{
-      '': {
-        target: 'localhost:8081',//代理地址，这里设置的地址会代替axios中设置的baseURL
-        changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
       }
     }
-    // proxy:{
-    //     '':{
-    //         target: 'http://localhost:8081',//代理地址，这里设置的地址会代替axios中设置的baseURL
-    //         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
-    //        }
-    // }}
   }
 }
