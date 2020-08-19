a77c3ea0b164774161bab55c5642dce21798a845
Lonnie <biubiu0_0@163.com>
2020-08-19 18:03:19 +0800

    原型有效性检测完善

--- ui/src/views/CreateTask.vue
+++ ui/src/views/CreateTask.vue
@@ -1,23 +1,29 @@
 <template>
   <div>
-    <div class>
-      <label for="pttUrl">原型链接</label>
-      <input
-        id="pttUrl"
-        v-model="pttUrl"
-        name="pttUrl"
-        type="url"
-      >
-      <template v-if = "pttUrl.trim() !== ''">
-        <span class="tip" v-if="urlIsValid">URL OK</span>
-        <span class="tip" v-else-if="iframeSrc !== ''" >URL 无效</span>
-      </template>
-    </div>
     <div>
-      <iframe
-        :src="iframeSrc"
-        frameborder="0"
-      />
+      <form @submit.prevent="submit">
+        <label for="ipturl">原型链接</label>
+        <input
+          id="ipturl"
+          v-model="ipturl"
+          name="ipturl"
+          type="url"
+          placeholder="http://protomanager.com/yourproto"
+          required
+        >
+        <button type="submit">确定</button>
+        <input type="checkbox" id="retry" v-model="retry">
+        <label for="retry">自动重试</label>
+      </form>
+      <template v-if = "ipturl.trim() !== ''">
+        <div class="tip" v-if="urlState !== ''" v-html="urlState"></div>
+        <div>
+          <iframe
+            :src="iframeSrc"
+            frameborder="0"
+          />
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -26,9 +32,11 @@
 export default {
   data () {
     return {
-      pttUrl: '',
+      ipturl: '',
       iframeSrc: '',
-      urlIsValid: false
+      urlState: '',
+      retry: false,
+      retryDur: 6000
     }
   },
   mounted () {
@@ -36,67 +44,77 @@ export default {
     window.addEventListener('message', this.receiveMsgFromWin, false)
   },
   watch: {
-    pttUrl: function () {
-      // 去空格
-      const url = this.pttUrl.trim()
-      // 如果为空或者没变, 则不管
-      if (url === '' || url === this.iframeSrc) return
-      // 若有修改则清除,重置定时器
-      if (this.lazyLoadTimer) clearTimeout(this.lazyLoadTimer)
-      if (this.reloadTimer) clearInterval(this.reloadTimer)
-      // 延时加载框架
-      this.lazyLoadTimer = setTimeout(this.lazyLoad, 1000, url)
+    ipturl () {
+      if (!this.ipturl.startsWith('http://') && !this.ipturl.startsWith('https://')) {
+        this.urlState = "地址应以 'http://'或'https://' 开头"
+      } else {
+        this.urlState = ''
+      }
     }
   },
   methods: {
+    submit () {
+      // 去空格
+      const url = this.ipturl.trim()
+      // 如果为空或者没变, 则不管
+      if (url === '' || url === this.iframeSrc) return false
+      if (!url.startsWith('http:') && !url.startsWith('https:')) return false
+
+      if (this.reloadTimer) clearInterval(this.reloadTimer)
+      this.urlState = '正在检查地址...'
+      this.iframeSrc = url
+      // 设定超时重试处理
+      this.reloadTimer = setInterval(this.reload, this.retryDur, url)
+    },
     receiveMsgFromWin (e) {
       if (this.iframeSrc === '') return
       const eData = e.data
       const pttOrigin = e.origin
       // 判断是否为用户输入的地址发送的消息
-      if (new URL(this.iframeSrc).origin === e.origin) {
-        if (this.Store.debug) { console.info('收到 %s 的消息： %o', pttOrigin, eData) }
-        this.urlIsValid = true
-        clearInterval(this.reloadTimer)
-        // 消息判断和处理
-        switch (eData.status) {
-          case 'init': {
-            this.iframeSrc = eData.url
-            break
-          }
-          case 'isReady': {
-            this.urlIsValid = true
-            break
-          }
-        }
-      }
-    },
-    timeReload (url) {
-      console.info('原型超时未应答')
-      this.urlIsValid = false
-      // iframe重载
-      this.iframeSrc = url
-    },
-    lazyLoad (url) {
       try {
-        let fullUrl = url
-        // 检查是否有协议标头
-        if (!fullUrl.startsWith('http:') && !fullUrl.startsWith('https:')) fullUrl = 'http://' + fullUrl
-        // 检查是否为有效的URL格式
-        const src = new URL(fullUrl)
-        // 设置框架链接, 立即加载
-        this.iframeSrc = src.href
-        // 超时循环加载
-        this.reloadTimer = setInterval(this.timeReload, 8000, src.href)
+        if (new URL(this.iframeSrc).origin === e.origin) {
+          if (this.Store.debug) { console.info('收到 %s 的消息： %o', pttOrigin, eData) }
+          // 消息判断和处理
+          switch (eData.status) {
+            case 'init': {
+              this.urlState = '原型地址定位中...'
+              this.iframeSrc = eData.url
+              clearInterval(this.reloadTimer)
+              break
+            }
+            case 'isReady': {
+              this.urlState = '原型已准备好, 可以开始创建任务'
+              clearInterval(this.reloadTimer)
+              break
+            }
+          }
+        } else if (this.Store.debug) console.log('消息来源不是用户输入的地址')
       } catch (error) {
-        // URL无效
-        this.iframeSrc = ''
-        this.urlIsValid = false
+        if (this.Store.debug) console.error(error)
       }
+    },
+    reload (url) {
+      // 如果不自动重试,则取消定时器
+      this.urlState = "此地址或许没有加入插件，<a href='/help'>如何给原型添加插件？</a>"
+      if (!this.retry) {
+        clearInterval(this.reloadTimer)
+        return
+      }
+      this.urlState += ', 正在刷新重试...'
+      // iframe重载
+      this.iframeSrc = ''
+      const _url = url
+      setTimeout(() => {
+        this.iframeSrc = _url
+      }, this.retryDur / 2)
     }
   }
 }
 </script>
 
 <style>
+iframe {
+  width: 100%;
+  height: 100%;
+}
 </style>
