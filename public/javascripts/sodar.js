3c4eb2b5b33a64afa8cbe3b933bafad044efa270
Lonnie <biubiu0_0@163.com>
2020-09-22 18:16:16 +0800

    手撸一个ajax方法

--- public/javascripts/sodar.js
+++ public/javascripts/sodar.js
@@ -1,16 +1,29 @@
 console.log('sodar.js 加载成功')
 var htmlHead = document.getElementsByTagName('head').item(0)
 
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "http://www.example.org/example.txt");
+oReq.send();
+
 // 任务管理和引导
 // 任务测试报告
 // 任务测试场景
 if(window.location.search.includes('testid')){
-    
+  var testid = window.location.search.match(/testid=(\w+)/i)[1]
+  // 检查id长度是否正确
+  console.info('todo: 获取任务前检查id长度是否正确')
+  console.log('获取测试任务:'+ testid); 
+  
+  // 加载测试插件
+  var script_sodar = document.createElement('script')
+  script_sodar.setAttribute("type", "text/javascript");
+  script_sodar.setAttribute('defer','defer')
+  script_sodar.src = 'http://localhost:8081/javascripts/monitor_f.js'
+  htmlHead.append(script_sodar)
 }
 
-// 加载测试插件
-var script_sodar = document.createElement('script')
-script_sodar.setAttribute("type", "text/javascript");
-script_sodar.setAttribute('defer','defer')
-script_sodar.src = 'http://localhost:8081/javascripts/monitor_f.js'
-htmlHead.append(script_sodar)
