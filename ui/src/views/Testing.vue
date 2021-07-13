fa75a0a0671a63024ecc4f1cd63c5e7407544e78
onlyzlc <zhoulongchun@live.com>
2021-07-13 23:31:13 +0800

    修改测试结束判断
    放弃自动结束功能;
    由参与者自行判断自己是否已完成任务，这样做的好处是便于识别界面设计的反馈是否足够。

--- ui/src/views/Testing.vue
+++ ui/src/views/Testing.vue
@@ -4,25 +4,27 @@
       title="欢迎参与体验"
       :vis="popup_start.vis"
     >
-      {{ popup_start.tip }}
+      {{ taskNote.description || "" }}
       <template v-slot:foot-right>
         <button @click="startTest">
           开始
         </button>
       </template>
     </ln-dialog>
-    <ln-dialog
+    <!-- <ln-dialog
       title="任务完成"
       :vis="isCompleted"
       @click-primary="NavToThk"
     >
       啊妹惊, 您完成了这个不可能的任务!!!
-    </ln-dialog>
+    </ln-dialog> -->
     <ln-dialog
-      title="不知如何继续了吗?"
+      title="遇到困难了吗?"
       :vis="popup_lost.vis"
       btnPrimary="提交"
+      btnSecondary="没什么,继续"
       @click-primary="feedback"
+      @click-secondary="popup_lost.vis=false"
     >
       <p>请说出您的疑惑:</p>
       <textarea name="" id="" cols="80" rows="5"></textarea>
@@ -36,6 +38,29 @@
         <div />
       </template>
     </ln-dialog>
+    <header>
+      <div class="title">
+        <div id="logo">
+          Sodar
+        </div>
+        <h4>目标: {{taskNote.description}}</h4>
+      </div>
+      <div class="control">
+        <button
+          @click="NavToThk"
+        >
+          已完成
+        </button>
+        <button @click="popup_lost.vis = true">
+          放弃
+        </button>
+      </div>
+      <div class="info">
+        <p>
+          机器人正在记录您在下方网页中的操作
+        </p>
+      </div>
+    </header>
     <ptt-frame
       :url="pttUrl"
       :status="status"
@@ -57,7 +82,6 @@ export default {
     return {
       taskId: this.$route.params.taskId,
       taskNote: {},
-      pttHost: ['http://127.0.0.1:8082'],
       status: 'ready',
       msg: {},
       pttUrl: '',
@@ -85,7 +109,11 @@ export default {
       .then((res) => {
         switch (res.status) {
           case 200:
-            this.store(res.data)
+            // this.store(res.data)
+            this.taskNote = res.data
+            this.popup_start.vis = true
+            this.stop = this.taskNote.steps[1] || this.taskNote.steps[0]
+            this.pttUrl = this.taskNote.steps[0].url
             break
           case 204:
             // 任务未发布
@@ -98,20 +126,16 @@ export default {
       })
   },
   methods: {
-    store (taskNote) {
-      this.pttUrl = taskNote.steps[0].url
-      this.popup_start.tip = taskNote.description
-      this.popup_start.vis = true
-      this.stop = taskNote.steps[1] || taskNote.steps[0]
-    },
     processCmd (cmd, content) {
       if (cmd === 'post') {
         clearTimeout(this.timer)
         this.timer = setTimeout(this.lost, this.lostTime)
         // 判断是否触发最后一步,到达最后一步时自动结束测试;
-        if (content.type === this.stop.type &&
+        if (content.url === this.stop.url &&
+            content.type === this.stop.type &&
             content.target.id === this.stop.target.id &&
             content.target.nodeName.toLowerCase() === this.stop.target.nodeName &&
+            content.target.innerText === this.stop.target.innerText &&
             content.target.value === this.stop.target.value) {
           this.isCompleted = true
           this.log.push(content)
@@ -143,11 +167,11 @@ export default {
     lost () {
       // 如果超过一定时间没有动作, 则结束测试并显示反馈对话框
       if (this.log.length) this.sendLog()
-      this.status = 'stop'
       this.popup_lost.vis = true
     },
     feedback () {
       // todo: 提交反馈
+      this.status = 'stop'
       this.NavToThk()
     },
     NavToThk () {
