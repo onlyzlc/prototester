35743fe56a4b8005dfa277b11aa050c3d70533a2
onlyzlc <zhoulongchun@live.com>
2022-07-15 13:32:14 +0800

    新增折叠组件

---
+++ ui/src/components/Ln-Folder.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <button @click="folded = !folded">
+        开/合
+      </button>
+      <slot name="label" ></slot>
+    </div>
+    <div
+      v-if="!folded"
+      class="panel"
+    >
+      <slot name="panel">
+        下拉面板
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['prop'],
+  data () {
+    return {
+      folded: true
+    }
+  }
+}
+</script>
+
+<style lang="">
+
+</style>
